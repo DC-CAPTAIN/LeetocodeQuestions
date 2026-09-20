03f7a3f82122c404b88ec212af7b9b0056953ee0
Kartikeya Lashkari <kartikeyalashkari@gmail.com>
2026-09-20 18:32:19 +0530

    Day 5 41. First Positive Integer

---
+++ HardQuestions/firstPositiveInteger/firstPositiveInteger.js
@@ -0,0 +1,26 @@
+var firstPositiveInteger = function (nums) {
+    let n = nums.length;
+    let i = 0;
+
+    while (i < n) {
+        let correctIndex = nums[i] - 1;
+        if (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[correctIndex]) {
+            swap(nums, i, correctIndex);
+        } else {
+            i++;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
+}
+
+var swap = function (nums, i, j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
